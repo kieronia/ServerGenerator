2375f9d8fec55a678b10d8c77ad6fbb852c384e0
Kieronia <70661699+kieronia@users.noreply.github.com>
2020-09-20 14:40:51 +0100

    Update Gen.py
    Make sure you type
    pip install requests
    into cmd and make sure to put your webhook url where it says 👍

--- Gen.py
+++ Gen.py
@@ -6,9 +6,9 @@ webhookurl = "https://discordapp.com/api/webhooks/123123/abcdefg" #WEBHOOK HERE
 while True:
     word = requests.get("https://random-word-api.herokuapp.com/word?number=1").text[2:-2]
     dataaa = {
-        "content" : f"discord.gg/{word}", #! Don't touch anything here tbh
-        "username" : "Discord.gg Server 'generator'", #! Username for webhook
-        "avatar_url" : "https://www.pngkit.com/png/detail/19-194063_discord-logo-discord-icon.png   ", #! Profile Picture For Webhook (eg. https://www.ascii.wtf/ascii.png)
+        "content" : f"discord.gg/{word}", #message for webhook to send
+        "username" : "Discord.gg Server 'generator'", #username for webhook
+        "avatar_url" : "https://www.pngkit.com/png/detail/19-194063_discord-logo-discord-icon.png   ", #pfp for webhook
     }
 
     requests.post(webhookurl, data=dataaa)
